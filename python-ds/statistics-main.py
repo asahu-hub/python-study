3c18360f22a16ce1308deee2adb213cca69461c5
unknown <akshaysahu@hotmail.com>
2020-07-01 13:29:10 -0600

    renamed packages

--- python-ds/statistics-main.py
+++
@@ -1,9 +0,0 @@
-import csv
-from statistics.sample_statisitcs_calculator import StatisticsCalculator
-
-with open('dataset/Dataset.csv', 'r') as csvfile:
-    dataset=csv.DictReader(csvfile)
-    dictDataset=list(dataset)
-    print('Total number of records: %d' % len(dictDataset))
-    print('Headers: ', len(dictDataset[0].keys()))
-    
