8998d14d72a43beca81c8a44a98491f695d746d4
unknown <akshaysahu@hotmail.com>
2020-09-02 02:18:12 -0600

    Added Python code to understand python better

--- py-collections/py-sets.py
+++ py-collections/py-sets.py
@@ -5,6 +5,9 @@ print(oddset)
 evenset={item for item in range(1, 15) if(item%2)==0}
 print(evenset)
 
+emptyset=set()
+print(type(emptyset))
+
 print('Mathematical Set Operations')
 print('A intersection B: {0}'.format(oddset & evenset))
 print('A union B: {0}'.format(oddset | evenset))
