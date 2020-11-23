73c436bfc825028c8af3ab91bf8d378b5ede9f9b
Akshay Sahu - GATech <asahu33@gatech.edu>
2020-11-22 22:52:52 -0700

    Added package for numpy computing

---
+++ py-matrices-vectors/py-numpy-intro.py
@@ -0,0 +1,9 @@
+import numpy as np
+'''
+1. Numpy is fundamental package for scientific computing in Python.
+2. The most important object of numpy is N-dimensional array object.
+3. It can be used to perform Linear Algebra, Fourier Transformation and Random Number operations.
+4. It is a building block for other mathematical packages (eg: scipy)
+5. It is an Open Source library.
+'''
+print("Numpy Version: ", np.__version__)
